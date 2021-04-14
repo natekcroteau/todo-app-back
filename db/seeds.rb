a82253edb34392f3222de75f448ef123f597d77f
natekcroteau <natekcroteau@gmail.com>
2021-04-14 14:19:48 -0600

    init backend with todo model resources and 3 seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Todo.destroy_all
+
+Todo.create(
+    title: "First todo",
+    content: "This is my first todo item",
+    urgent: false,
+    done: false
+)
+Todo.create(
+    title: "Second todo",
+    content: "This is my Second todo item",
+    urgent: true,
+    done: false
+)
+Todo.create(
+    title: "Third todo",
+    content: "This is my Third todo item",
+    urgent: false,
+    done: false
+)

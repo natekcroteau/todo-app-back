d11e513725440bf0015497512008e015a5f34f5f
natekcroteau <natekcroteau@gmail.com>
2021-04-18 08:49:15 -0600

    added update resources

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,24 +1,39 @@
 class TodosController < ApplicationController
 
+
     def index 
         @todos = Todo.all 
         render json: @todos
     end
 
+
     def create
-        @todo = Todo.create(
-            title: params[:title],
-            content: params[:content],
-            urgent: params[:urgent],
-            done: params[:done]
-        )
+        @todo = Todo.create(todo_params)
         render json: @todo, status: :created
     end
+
+
+    def update
+        @todo = Todo.find(params[:id])
+        @todo.update(todo_params)
+        render json: @todo
+    end
+
 
     def destroy
         @todo = Todo.find(params[:id])
         @todo.destroy
         render status: :no_content
+    end
+
+
+
+
+    private
+
+    
+    def todo_params
+        params.require(:todo).permit(:title, :content, :urgent, :done)
     end
 
 

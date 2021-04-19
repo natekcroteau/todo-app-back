9bf776ae924dae4595ca90ab9d3a9ff1b03e766e
natekcroteau <natekcroteau@gmail.com>
2021-04-19 12:21:41 -0600

    adjusted error message response for user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,16 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-    
         if @user.valid?
             @user.save
             @token = JWT.encode({user_id: @user.id}, 'secret')
             render json: {user: @user, token: @token}, status: :created
         else
-            render json: {error: 'Username or Password Invalid'}, status: :not_acceptable
+            render json: {errors: @user.errors.full_messages}, status: :not_acceptable
         end
     end
 
-    
-    
+
     
     private
 
